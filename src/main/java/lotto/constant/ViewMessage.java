110e08ea360ddfc26c7b9caeb875854217e2fb70
yeo-li <81564503+yeo-li@users.noreply.github.com>
2024-11-22 20:31:25 +0900

    feat(InputView, InputMessage): 당첨 번호 입력 기능 구현

--- src/main/java/lotto/constant/ViewMessage.java
+++ src/main/java/lotto/constant/ViewMessage.java
@@ -1,7 +1,8 @@
 package lotto.constant;
 
 public enum ViewMessage {
-    PROMPT_AMOUNT("구입 금액을 입력해 주세요");
+    PROMPT_AMOUNT("구입 금액을 입력해 주세요"),
+    PROMPT_WINNING_NUMBER("당첨 번호를 입력해 주세요.");
 
 
     private String viewMessage;
--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -8,4 +8,9 @@ public class InputView {
         System.out.println(ViewMessage.PROMPT_AMOUNT.getViewMessage());
         return Console.readLine();
     }
+
+    public String inputWinningNumber() {
+        System.out.println(ViewMessage.PROMPT_WINNING_NUMBER.getViewMessage());
+        return Console.readLine();
+    }
 }
